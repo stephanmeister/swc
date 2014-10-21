b6b3ee847288e7d8b0a3cba4a75287190d13ac87
Stephan Meister <github@stephanmeister.com>
2014-10-21 11:21:05 -0700

    added bash script to curl & unpack mosquito data

---
+++ pipeline.sh
@@ -0,0 +1,32 @@
+echo --- cleaning up ---
+rm *.txt
+rm *.tmp
+rm *.csv
+rm *.zip
+
+echo 
+echo --- downloading data ---
+curl https://github.com/zonca/swcarpentry-workshop-pandas/blob/master/rawdata/rawdata.zip?raw=true -o rawdata.zip -L
+
+echo 
+echo --- unpacking data ---
+unzip rawdata.zip
+
+echo 
+echo --- removing temp files ---
+rm *.tmp
+
+echo 
+echo --- renaming ---
+for f in *.txt
+do
+	mv $f ${f/txt/csv}
+done
+
+echo 
+echo --- removing zip file ---
+rm rawdata.zip
+
+echo 
+echo --- Available files ---
+ls *.csv
